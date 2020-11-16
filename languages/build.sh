7e2e434f9ab1731c34c029adc95cd54fa34f963d
Mihai Borobocea <138839+MihaiB@users.noreply.github.com>
2020-11-16 21:06:40 +0200

    add languages: basic document mentioning different languages (#2)

--- languages/build.sh
+++ languages/build.sh
@@ -5,7 +5,14 @@ trap "echo >&2 script '${BASH_SOURCE[0]}' failed" ERR
 
 SCRIPT=`readlink -e "${BASH_SOURCE[0]}"`
 SCRIPT_DIR=`dirname "$SCRIPT"`
-cd "$SCRIPT_DIR"
-unset SCRIPT SCRIPT_DIR
 
-languages/build.sh
+cd "$SCRIPT_DIR"
+FILE_BASE=languages
+for I in {1..2}; do
+	lualatex "$FILE_BASE".tex
+done
+rm \
+	"$FILE_BASE".aux \
+	"$FILE_BASE".log \
+	"$FILE_BASE".out \
+	#
